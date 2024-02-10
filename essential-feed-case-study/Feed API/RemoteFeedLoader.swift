5647c4e8f1d1a5d0b4cb04c54d16c0199b9f261b
gtsofa <jlsnyule@yahoo.com>
2024-02-10 17:17:42 +0300

    Move FeedItemsMapper to its own file

--- essential-feed-case-study/Feed API/RemoteFeedLoader.swift
+++ essential-feed-case-study/Feed API/RemoteFeedLoader.swift
@@ -11,7 +11,6 @@ public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
     
-    
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
@@ -42,34 +41,6 @@ public final class RemoteFeedLoader {
                 completion(.failure(.connectivity))
             }
         }
-    }
-}
-
-private class FeedItemsMapper {
-    private struct Root: Decodable {
-        let items: [Item]
-    }
-
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            return FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
-    }
-    
-    static var OK_200: Int { return 200 }
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.items.map {$0.item }
     }
 }
 
