ceae98b1622c0aee820d987afa621fb188b50c76
gtsofa <jlsnyule@yahoo.com>
2024-02-19 13:08:28 +0300

    Add nil data and nil response to the request error case to make the setup more explicit

--- essential-feed-case-studyTests/Feed API/URLSessionHTTPClientTests.swift
+++ essential-feed-case-studyTests/Feed API/URLSessionHTTPClientTests.swift
@@ -35,7 +35,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         let error = NSError(domain: "any error", code: 1)
         //stub url + task
-        URLProtocolStub.stub(url: url, error: error)
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
         
         let exp = expectation(description: "Wait for load completion")
         sut.get(from: url) { result in
@@ -61,11 +61,13 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         //combine task + error i.e we can use a tuple or a struct
         private struct Stub {
+            let data: Data?
+            let response: URLResponse?
             let error: Error?
         }
         
-        static func stub(url: URL, error: Error? = nil) {
-            URLProtocolStub.stubs[url] = Stub(error: error)
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
+            URLProtocolStub.stubs[url] = Stub(data: data, response: response, error: error)
         }
         
         static func startInterceptingRequests() {
@@ -89,6 +91,14 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         override func startLoading() {
             guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
+            
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
             
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
