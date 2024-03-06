a72bfa82bbff84a1ddaefa1a6cd8f406b98e0111
gtsofa <jlsnyule@yahoo.com>
2024-03-06 10:41:47 +0300

    Save commands requests cache deletion

--- essential-feed-case-studyTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ essential-feed-case-studyTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,12 +6,18 @@
 //
 
 import XCTest
+import essential_feed_case_study
 
 class LocalFeedLoader {
     let store: FeedStore
     
     init(store: FeedStore) {
         self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        //need to invoke a mtd
+        store.deleteCachedFeed()
     }
 }
 
@@ -21,15 +27,41 @@ class LocalFeedLoader {
 //making sure we don't leak framework details into the use case
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    // mtd to be invoked in sut
+    // implements the behavior we expect
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
-    //w/o invoking any behaviour
+    // w/o invoking any behaviour
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
+    
+    // factory method
+    // why prefer function?
+    func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
     }
 
 }
