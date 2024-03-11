2b9629f21c77b1d652bff7ffe09e69205ba09fe4
gtsofa <jlsnyule@yahoo.com>
2024-03-11 19:03:14 +0300

    Extract `FeedStoreSpy` to a shared scope to remove duplication

--- essential-feed-case-studyTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ essential-feed-case-studyTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import essential_feed_case_study
+
+class FeedStoreSpy: FeedStore {
+    
+    enum ReceivedMessage: Equatable {
+        case deleteCachedFeed
+        case insert([LocalFeedImage], Date)
+    }
+    // combine all msgs
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    // use typealias for readability
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    // mtd to be invoked in sut
+    // implements the behavior we expect
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        // then we capture the completions
+        deletionCompletions.append(completion)
+        //incase of the above
+        receivedMessages.append(.deleteCachedFeed)
+    }
+    
+    func completeDeletion(with error: NSError, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        // we capture items and timestam msgs :)
+        receivedMessages.append(.insert(feed, timestamp))
+    }
+    
+    func completeInsertion(with error: NSError, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+}
