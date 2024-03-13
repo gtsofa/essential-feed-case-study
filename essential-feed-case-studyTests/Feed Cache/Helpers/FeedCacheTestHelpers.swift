5d10221abf9affd173958e6d85fd8f49e52ea3e9
gtsofa <jlsnyule@yahoo.com>
2024-03-13 11:53:03 +0300

    Extract duplicate test helpers into a shared scope

---
+++ essential-feed-case-studyTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  FeedCacheTestHelpers.swift
+//  essential-feed-case-studyTests
+//
+//  Created by Julius on 13/03/2024.
+//
+
+import Foundation
---
+++ essential-feed-case-studyTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  SharedTestHelpers.swift
+//  essential-feed-case-studyTests
+//
+//  Created by Julius on 13/03/2024.
+//
+
+import Foundation
