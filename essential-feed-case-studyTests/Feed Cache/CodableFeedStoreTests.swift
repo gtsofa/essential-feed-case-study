134b720d91f728e8785907fc57718c5b326ee5b4
gtsofa <jlsnyule@yahoo.com>
2024-03-18 13:42:30 +0300

    Add helpers methods to provide documentation, context and clarify test `setUp` and `tearDown` intent regarding side-effects

--- essential-feed-case-studyTests/Feed Cache/CodableFeedStoreTests.swift
+++ essential-feed-case-studyTests/Feed Cache/CodableFeedStoreTests.swift
@@ -76,12 +76,13 @@ final class CodableFeedStore {
 final class CodableFeedStoreTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        undoStoreSideEffects()
     }
     
     override func setUp() {
         super.setUp()
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        setupEmptyStoreState()
+        
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -156,6 +157,18 @@ final class CodableFeedStoreTests: XCTestCase {
         let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     private func testSpecificStoreURL() -> URL {
