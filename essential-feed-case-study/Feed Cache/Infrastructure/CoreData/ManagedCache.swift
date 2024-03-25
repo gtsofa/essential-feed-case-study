757e95f8da963ee1b516d0f797468c41d62b9ec6
gtsofa <jlsnyule@yahoo.com>
2024-03-26 00:24:24 +0300

    Extract CoreData managed model classes into separate files

---
+++ essential-feed-case-study/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,8 @@
+//
+//  ManagedCache.swift
+//  essential-feed-case-study
+//
+//  Created by Julius on 26/03/2024.
+//
+
+import Foundation
---
+++ essential-feed-case-study/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -0,0 +1,8 @@
+//
+//  ManagedFeedImage.swift
+//  essential-feed-case-study
+//
+//  Created by Julius on 26/03/2024.
+//
+
+import Foundation
