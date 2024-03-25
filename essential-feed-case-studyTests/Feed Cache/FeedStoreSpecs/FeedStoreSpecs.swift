93e33aa9512c3c4e0e4deb485921e20dd15b70bd
gtsofa <jlsnyule@yahoo.com>
2024-03-25 11:24:36 +0300

    Refine the FeedStoreSpecs by adding missing tests

--- essential-feed-case-studyTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
+++ essential-feed-case-studyTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
@@ -14,9 +14,13 @@ protocol FeedStoreSpecs {
     func test_retrieve_deliversFoundValuesOnNonEmptyCache()
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
 
+    func test_insert_deliversNoErrorOnEmptyCache()
+    func test_insert_deliversNoErrorOnNonEmptyCache()
     func test_insert_overridesPreviouslyInsertedCacheValues()
     
+    func test_delete_deliversNoErrorOnEmtpyCache()
     func test_delete_hasNoSideEffectsOnEmtpyCache()
+    func test_delete_deliversNoErrorOnNonEmptyCache()
     func test_delete_emptiesPreviouslyInsertedCache()
     
     func test_storeSideEffects_runSerially()
