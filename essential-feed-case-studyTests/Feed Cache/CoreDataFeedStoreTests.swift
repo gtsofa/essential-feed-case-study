dfa02a563b30b6e3eee697f41b91b63724fb33dd
gtsofa <jlsnyule@yahoo.com>
2024-03-25 22:32:53 +0300

    `CoreDataFeedStore.insert()` delivers no error on empty cache

--- essential-feed-case-studyTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ essential-feed-case-studyTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -33,7 +33,11 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
         assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
     }
 
-    func test_insert_deliversNoErrorOnEmptyCache() {}
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+    }
     
     func test_insert_deliversNoErrorOnNonEmptyCache() {}
     
