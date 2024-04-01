5d61f91bb6e720b2c20b470835edd298201844ce
gtsofa <jlsnyule@yahoo.com>
2024-04-01 23:02:27 +0300

    Add fade-in animation to simulate "asynchronous image loading"

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -44,6 +44,7 @@ extension FeedImageCell {
         descriptionLabel.text = model.description
         descriptionLabel.isHidden = model.description == nil
         
-        feedImageView.image = UIImage(named: model.imageName)
+        fade(UIImage(named: model.imageName))
+        //feedImageView.image = UIImage(named: model.imageName)
     }
 }
