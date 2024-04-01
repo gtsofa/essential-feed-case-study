5d7bff6a0a645189896d9970b331bb54e49c6395
gtsofa <jlsnyule@yahoo.com>
2024-04-01 22:36:05 +0300

    Add `FeedImageCell` with storyboard connections

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Julius on 01/04/2024.
+//
+
+import UIKit
+
+class FeedImageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
