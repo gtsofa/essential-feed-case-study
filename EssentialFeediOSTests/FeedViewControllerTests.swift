e9e9939bd5e4e66124e624082c2a618c89cd72de
gtsofa <jlsnyule@yahoo.com>
2024-04-12 17:31:19 +0300

    add interpretation

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -133,6 +133,7 @@ private extension FeedViewController {
 }
 
 private extension UIRefreshControl {
+    // Read all of the targets for UIRefreshControl (in our instance there is only one target which is self) and for any defined .valueChanged events perform the selector that was assigned to the target for the event.
     func simulatePullToRefresh() {
         allTargets.forEach{ target in
             actions(forTarget: target, forControlEvent:
