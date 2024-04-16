648c37d0030284000559a7798ea30ba81fc066bc
gtsofa <jlsnyule@yahoo.com>
2024-04-16 13:58:46 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Julius on 16/04/2024.
+//
+
+import UIKit
+
+class FeedImageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
