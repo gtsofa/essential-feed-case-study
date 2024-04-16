52cacff10bebe4d811952637d836eafdf85d3c4f
gtsofa <jlsnyule@yahoo.com>
2024-04-16 19:07:32 +0300

    Extract `cancelImageDataLoad(from: URL)` method from `FeedImageDataLoader` protocol into a new `FeedImageDataLoaderTask` protocol that represents a task that can be cancelled. This way, we respect the Interface Segregation Principle and `FeedImageDataLoader` implementations are not forced to be stateful.

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 import essential_feed_case_study
 
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
 public protocol FeedImageDataLoader {
-    func loadImageData(from url: URL) // hold states
-    func cancelImageDataLoad(from url: URL) // cancel the state
+    func loadImageData(from url: URL) -> FeedImageDataLoaderTask
 }
 
 final public class FeedViewController: UITableViewController {
@@ -19,6 +22,7 @@ final public class FeedViewController: UITableViewController {
     private var isViewAppeared = false
     private var onViewIsAppearing: ((FeedViewController) -> Void)?
     private var tableModel = [FeedImage]()
+    private var tasks = [IndexPath: FeedImageDataLoaderTask]() // feedvc keeps track of this tasks in a dictionary
     
     public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
         self.init()
@@ -67,13 +71,15 @@ final public class FeedViewController: UITableViewController {
         cell.locationContainer.isHidden = (cellModel.location == nil) // location container is hidden when data.location==nil
         cell.locationLabel.text = cellModel.location// location text is data.location
         cell.descriptionLabel.text = cellModel.description // location text is data.description
-        imageLoader?.loadImageData(from: cellModel.url)
+        // store a task for a given indexpath
+        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) // keep track of the state now
         return cell
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         // tells imageloader to cancel an image data load from a url
-        let cellModel = tableModel[indexPath.row]
-        imageLoader?.cancelImageDataLoad(from: cellModel.url)
+        // cancel a task for agiven indexpath
+        tasks[indexPath]?.cancel()
+        tasks[indexPath] = nil
     }
 }
