d8cd4e29e4c5c2ef1025f62bf0c05025427c7aab
gtsofa <jlsnyule@yahoo.com>
2024-04-19 23:14:02 +0300

    Extract FeedViewController test helpers to a separate file

--- EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -401,7 +401,7 @@ final class FeedViewControllerTests: XCTestCase {
     }
 }
 
-private class FakeRefreshControl: UIRefreshControl {
+class FakeRefreshControl: UIRefreshControl {
     private var _isRefreshing = false
     
     override var isRefreshing: Bool { _isRefreshing }
@@ -412,89 +412,6 @@ private class FakeRefreshControl: UIRefreshControl {
     
     override func endRefreshing() {
         _isRefreshing = false
-    }
-}
-
-//FeedViewController
-private extension FeedViewController {
-    
-    var isShowingLoadingIndicator: Bool {
-        refreshControl?.isRefreshing == true
-    }
-    
-    func simulateFeedImageViewNotNearVisible(at row: Int = 0) {
-        simulateFeedImageViewVisible(at: row)
-        let ds = tableView.prefetchDataSource
-        let index = IndexPath(row: row, section: feedImagesSection)
-        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
-        
-    }
-    
-    func simulateFeedImageViewNearVisible(at row: Int = 0) {
-        let ds = tableView.prefetchDataSource
-        let index = IndexPath(row: row, section: feedImagesSection)
-        ds?.tableView(tableView, prefetchRowsAt: [index])
-    }
-    
-    func simulateUserInitiatedFeedReload() {
-        refreshControl?.simulatePullToRefresh()
-    }
-    
-    @discardableResult
-    func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
-        return feedImageView(at: index) as? FeedImageCell // instantiate teh image view that is created from the data source taht should trigger an image url request
-    }
-    
-    func simulateFeedImageViewNotVisible(at row: Int) {
-        let view = simulateFeedImageViewVisible(at: row) // simulate the view as visible
-        
-        let delegate = tableView.delegate // use the ableview delegate to notify 'didEndDisplay'
-        let index = IndexPath(row: row, section: feedImagesSection)
-        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
-    }
-    
-    func numberOfRenderedFeedImageViews() -> Int {
-        return tableView.numberOfRows(inSection: feedImagesSection)
-    }
-    
-    func feedImageView(at row: Int) -> UITableViewCell? {
-        let ds = tableView.dataSource// gets data source
-        let index = IndexPath(row: row, section: feedImagesSection)// create index path for a given row
-        return ds?.tableView(tableView, cellForRowAt: index) // ask for data source for the cell at that index
-    }
-    
-    private var feedImagesSection: Int {
-        return 0
-    }
-    
-    func simulateAppearance() {
-        if !isViewLoaded {
-            loadViewIfNeeded()
-            prepareForFirstAppearance()
-            //replaceRefreshControlWithFakeForiOS17Support()
-        }
-        beginAppearanceTransition(true, animated: false)
-        endAppearanceTransition()
-    }
-    
-    private func prepareForFirstAppearance() {
-        setSmallFrameToPreventRenderingCells()
-        replaceRefreshControlWithFakeForiOS17Support()
-    }
-    
-    private func setSmallFrameToPreventRenderingCells() {
-        tableView.frame = CGRect(x: 0, y: 0, width: 390, height: 1)
-    }
-    
-    func replaceRefreshControlWithFakeForiOS17Support() {
-        let fake = FakeRefreshControl()
-        refreshControl?.allTargets.forEach{ target in
-            refreshControl?.actions(forTarget: target, forControlEvent:
-                    .valueChanged)?.forEach { action in
-                        fake.addTarget(target, action: Selector(action), for: .valueChanged)
-                    }
-        }
-        refreshControl = fake
     }
 }
 
