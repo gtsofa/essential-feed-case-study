106b6adc69ba8785fea4a57dc178aac58ef2e806
gtsofa <jlsnyule@yahoo.com>
2024-04-19 23:24:52 +0300

    Extract LoaderSpy to a separate file

--- EssentialFeediOSTests/Controllers/Helpers/FeedViewControllerTests+LoaderSpy.swift
+++ EssentialFeediOSTests/Controllers/Helpers/FeedViewControllerTests+LoaderSpy.swift
@@ -5,4 +5,64 @@
 //  Created by Julius on 19/04/2024.
 //
 
-import Foundation
+import UIKit
+import EssentialFeediOS
+import essential_feed_case_study
+
+class LoaderSpy: FeedLoader, FeedImageDataLoader {
+    // MARK: - FeedLoader
+    
+    private var feedRequests = [(FeedLoader.Result) -> Void ]()
+    
+    
+    var loadFeedCallCount: Int {
+        return feedRequests.count
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        feedRequests.append(completion)
+    }
+    
+    func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
+        feedRequests[index](.success(feed))
+    }
+    
+    func completeFeedLoadingWithError(at index: Int = 0) {
+        let error = NSError(domain: "an error", code: 0)
+        feedRequests[index](.failure(error))
+    }
+    
+    // MARK: - FeedImageDataLoader
+    
+    private struct TaskSpy: FeedImageDataLoaderTask {
+        let cancelCallback: () -> Void
+        
+        func cancel() {
+            cancelCallback()
+        }
+    }
+    
+    private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void) ]()
+    
+    // loaderspy is capturing loadedimageurls
+    var loadedImageURLs: [URL] {
+        return imageRequests.map {$0.url}
+    }
+    
+    //spy is capturing cancelledimageURLs
+    private(set) var cancelledImageURLs = [URL]()
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        imageRequests.append((url, completion))
+        return TaskSpy { [weak self] in self?.cancelledImageURLs.append(url)}
+    }
+    
+    func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
+        imageRequests[index].completion(.success(imageData))
+    }
+    
+    func completeImageLoadingWithError(at index: Int = 0) {
+        let error = NSError(domain: "an error", code: 0)
+        imageRequests[index].completion(.failure(error))
+    }
+}
