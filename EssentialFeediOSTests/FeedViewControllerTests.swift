ea375dd51ab6cdbfcb1915145fd69b069baf4017
gtsofa <jlsnyule@yahoo.com>
2024-04-19 22:40:54 +0300

    Extract UIRefreshControl test helper to a separate file

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -537,15 +537,3 @@ private extension UIButton {
         }
     }
 }
-
-private extension UIRefreshControl {
-    // Read all of the targets for UIRefreshControl (in our instance there is only one target which is self) and for any defined .valueChanged events perform the selector that was assigned to the target for the event.
-    func simulatePullToRefresh() {
-        allTargets.forEach{ target in
-            actions(forTarget: target, forControlEvent:
-                    .valueChanged)?.forEach {
-                        (target as NSObject).perform(Selector($0))
-                    }
-        }
-    }
-}
--- EssentialFeediOSTests/Helpers/UIRefreshControl+TestHelpers.swift
+++ EssentialFeediOSTests/Helpers/UIRefreshControl+TestHelpers.swift
@@ -5,4 +5,16 @@
 //  Created by Julius on 19/04/2024.
 //
 
-import Foundation
+import UIKit
+
+extension UIRefreshControl {
+    // Read all of the targets for UIRefreshControl (in our instance there is only one target which is self) and for any defined .valueChanged events perform the selector that was assigned to the target for the event.
+    func simulatePullToRefresh() {
+        allTargets.forEach{ target in
+            actions(forTarget: target, forControlEvent:
+                    .valueChanged)?.forEach {
+                        (target as NSObject).perform(Selector($0))
+                    }
+        }
+    }
+}
