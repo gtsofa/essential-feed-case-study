c335fa46d81277bdc54fb5c7c62bb18e56a3d1bb
gtsofa <jlsnyule@yahoo.com>
2024-04-25 23:11:27 +0300

    Memory management to the Composer layer. The `FeedPresenter` shouldn't have to know or handle their dependencies lifetime.

--- EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -8,7 +8,7 @@
 import Foundation
 import essential_feed_case_study
 
-protocol FeedLoadingView: class  {
+protocol FeedLoadingView  {
     func display(isLoading: Bool)
 }
 
@@ -28,7 +28,7 @@ final class FeedPresenter {
     
     //replace observable properties with the feedview protocol
     var feedView: FeedView?
-    weak var loadingView: FeedLoadingView?
+    var loadingView: FeedLoadingView?
 
     func loadFeed() {
         loadingView?.display(isLoading: true)
