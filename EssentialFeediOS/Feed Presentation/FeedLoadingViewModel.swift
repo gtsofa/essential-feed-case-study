b51b832be9007540f3517c127e2f451709458873
gtsofa <jlsnyule@yahoo.com>
2024-04-26 19:38:59 +0300

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Julius on 26/04/2024.
+//
+
+import Foundation
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Julius on 26/04/2024.
+//
+
+import Foundation
