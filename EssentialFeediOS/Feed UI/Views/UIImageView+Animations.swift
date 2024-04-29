e2869903b0a4926ac13eaf8f6c35403bf2ae4ce7
gtsofa <jlsnyule@yahoo.com>
2024-04-29 23:18:48 +0300

    Display image with animation

--- EssentialFeediOS/Feed UI/Views/UIImageView+Animations.swift
+++ EssentialFeediOS/Feed UI/Views/UIImageView+Animations.swift
@@ -5,4 +5,18 @@
 //  Created by Julius on 29/04/2024.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
