2006a16302b170ab5376b79e2ee688dcb4ad8872
gtsofa <jlsnyule@yahoo.com>
2024-05-01 14:50:40 +0300

    Move  to separate file

---
+++ EssentialFeediOS/Feed UI/Composers/FeedViewAdapter.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by Julius on 01/05/2024.
+//
+
+import Foundation
---
+++ EssentialFeediOS/Feed UI/Composers/WeakRefVirtualProxy.swift
@@ -0,0 +1,27 @@
+//
+//  WeakRefVirtualProxy.swift
+//  EssentialFeediOS
+//
+//  Created by Julius on 01/05/2024.
+//
+import UIKit
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
