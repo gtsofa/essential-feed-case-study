8acf0283f117ee177371e7f37bdc9ea07e01fc01
gtsofa <jlsnyule@yahoo.com>
2024-05-03 22:46:40 +0300

    Move `FeedLoadingViewModel` to separate file

--- essential-feed-case-study/Feed Presentation/FeedPresenter.swift
+++ essential-feed-case-study/Feed Presentation/FeedPresenter.swift
@@ -27,10 +27,6 @@ public protocol FeedLoadingView  {
     func display(_ viewModel: FeedLoadingViewModel)
 }
 
-public struct FeedLoadingViewModel {
-    public let isLoading: Bool
-}
-
 public protocol FeedErrorView {
     func display(_ viewModel: FeedErrorViewModel)
 }
