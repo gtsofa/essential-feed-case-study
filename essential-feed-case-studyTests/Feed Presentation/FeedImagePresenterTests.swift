4a683f7b4f591ba8ef74c9e2df1068a6fdbe3929
gtsofa <jlsnyule@yahoo.com>
2024-05-03 23:50:47 +0300

    `FeedImagePresenter` does not send messages to view on init

--- essential-feed-case-studyTests/Feed Presentation/FeedImagePresenterTests.swift
+++ essential-feed-case-studyTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -7,29 +7,31 @@
 
 import XCTest
 
+class FeedImagePresenter {
+    init(view: Any) {}
+}
+
 final class FeedImagePresenterTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_doesNotSendMessagesToView() {
+        let (_, view) = makeSUT()
+        
+        XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    // MARK: Helpers
+    
+    private func makeSUT(file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, view)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    private class ViewSpy {
+        let messages = [Any]()
     }
 
 }
