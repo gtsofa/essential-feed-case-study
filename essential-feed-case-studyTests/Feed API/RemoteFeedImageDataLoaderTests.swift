63ecc408a3d8d94ad14ffd0fb76352582490650d
gtsofa <jlsnyule@yahoo.com>
2024-05-07 17:50:27 +0300

    `RemoteFeedImageDataLoader` does not perform any URL request

--- essential-feed-case-studyTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ essential-feed-case-studyTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -7,29 +7,30 @@
 
 import XCTest
 
+class RemoteFeedImageDataLoader {
+    init(client: Any) {}
+}
+
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_doesNotPerformAnyURLRequest() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    // MARK:- Helper function
+    
+    private func makeSUT(url: URL = anyURL(), file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, client)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    private class HTTPClientSpy {
+        let requestedURLs = [URL]()
     }
 
 }
