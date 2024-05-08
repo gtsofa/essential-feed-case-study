d2a4cd955e67f92e6525f4f803db44f7e3962cfe
gtsofa <jlsnyule@yahoo.com>
2024-05-08 21:39:59 +0300

    Extract duplicate `HTTPURLResponse.statusCode == 200` validation into a `isOK` helper method

--- essential-feed-case-study/Feed API/Helpers/HTTPURLResponse+StatusCode.swift
+++ essential-feed-case-study/Feed API/Helpers/HTTPURLResponse+StatusCode.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension HTTPURLResponse {
+    private static var OK_200: Int { return 200 }
+    
+    var isOK: Bool {
+        return statusCode == HTTPURLResponse.OK_200
+    }
+}
