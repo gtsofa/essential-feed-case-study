762250d38237360ad0c4dafc3724f8fecb2944bb
gtsofa <jlsnyule@yahoo.com>
2024-05-08 21:10:39 +0300

    Extract duplicate ephemeral HTTPClient  creation into a factory helper

--- essential_feed_case_studyAPIEndToEndTests/essential_feed_case_studyAPIEndToEndTests.swift
+++ essential_feed_case_studyAPIEndToEndTests/essential_feed_case_studyAPIEndToEndTests.swift
@@ -51,13 +51,12 @@ final class essential_feed_case_studyAPIEndToEndTests: XCTestCase {
     // MARK: - Helpers
     
     private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
-        let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
-        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let loader = RemoteFeedImageDataLoader(client: client)
-        trackForMemoryLeaks(client, file: file, line: line)
+        let loader = RemoteFeedImageDataLoader(client: ephemeralClient())
+        
         trackForMemoryLeaks(loader, file: file, line: line)
         
         let exp = expectation(description: "Wait for load completion")
+        let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
         
         var receivedResult: FeedImageDataLoader.Result?
         _ = loader.loadImageData(from: url) { result in
@@ -75,9 +74,8 @@ final class essential_feed_case_studyAPIEndToEndTests: XCTestCase {
     // LoadFeedResult? koz it may timeout or fail
     private func getFeedResult(file: StaticString = #filePath,
                                line: UInt = #line) -> FeedLoader.Result? {
-        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let loader = RemoteFeedLoader(url: feedTestServerURL, client: client)
-        trackForMemoryLeaks(client, file: file, line: line)
+        let loader = RemoteFeedLoader(url: feedTestServerURL, client: ephemeralClient())
+        
         trackForMemoryLeaks(loader, file: file, line: line)
         
         let exp = expectation(description: "Wait for load completion")
@@ -90,6 +88,12 @@ final class essential_feed_case_studyAPIEndToEndTests: XCTestCase {
         wait(for: [exp], timeout: 5.0)
         
         return receivedResult
+    }
+    
+    private func ephemeralClient(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        trackForMemoryLeaks(client, file: file, line: line )
+        return client
     }
     
     private func expectedImage(at index: Int) -> FeedImage {
