07c753c3abc9adfc00de768a7adc5a322d564335
gtsofa <jlsnyule@yahoo.com>
2024-05-08 20:49:09 +0300

    Add `RemoteFeedImageDataLoader` end-to-end test to guarantee both the client(app) and the server (backend) respect the API contract, so we can fetch image data from remote

--- essential_feed_case_studyAPIEndToEndTests/essential_feed_case_studyAPIEndToEndTests.swift
+++ essential_feed_case_studyAPIEndToEndTests/essential_feed_case_studyAPIEndToEndTests.swift
@@ -35,7 +35,38 @@ final class essential_feed_case_studyAPIEndToEndTests: XCTestCase {
         }
     }
     
+    func test_EndToEndTestServerGETFeedImageDataResult_matchesFixedAccountData() {
+        switch getFeedImageDataResult() {
+        case let .success(data)?:
+            XCTAssertFalse(data.isEmpty, "Expected non-empty image data")
+            
+        case let .failure(error)?:
+            XCTFail("Expected successful image data result, got \(error) instead")
+            
+        default:
+            XCTFail("Expected successful image data result, got no result instead.")
+        }
+    }
+    
     // MARK: - Helpers
+    
+    private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
+        let testServer = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let loader = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: FeedImageDataLoader.Result?
+        _ = loader.loadImageData(from: testServer) { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        return receivedResult
+    }
     
     // LoadFeedResult? koz it may timeout or fail
     private func getFeedResult(file: StaticString = #filePath,
