3a305635ec77c0c6c98e3e6af1b91e7beab063c9
gtsofa <jlsnyule@yahoo.com>
2024-05-10 22:55:23 +0300

    Remove commmented code

--- essential-feed-case-study/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
+++ essential-feed-case-study/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -14,11 +14,6 @@ extension CoreDataFeedStore: FeedImageDataStore {
                 return try ManagedFeedImage.first(with: url, in: context)?.data
             })
         }
-        //        perform { context in
-        //            completion(Result {
-        //                return try ManagedFeedImage.first(with: url, in: context)?.data
-        //            })
-        //        }
     }
     
     public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
@@ -28,11 +23,5 @@ extension CoreDataFeedStore: FeedImageDataStore {
             image.data = data
             try? context.save()
         }
-        //        perform { context in
-        //            guard let image = try? ManagedFeedImage.first(with: url, in: context) else { return }
-        //
-        //            image.data = data
-        //            try? context.save()
-        //        }
     }
 }
