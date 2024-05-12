b634fcda02cc2c8dff3d19bf5eeeebb421c15131
gtsofa <jlsnyule@yahoo.com>
2024-05-12 14:55:36 +0300

    Move `CoreDataFeedStore` implementation of the `FeedStore` protocol to a separate file

--- essential-feed-case-study/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ essential-feed-case-study/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+extension CoreDataFeedStore: FeedStore {
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        perform { context in
+            completion(Result {
+                try ManagedCache.find(in: context).map(context.delete).map(context.save)
+            })
+        }
+    }
+    
+    public func insert(_ feed: [essential_feed_case_study.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        perform { context in
+            completion(Result {
+                // create a contex
+                //save the context
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
+                
+                // try to save context
+                try context.save()
+            })
+        }
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        perform { context in
+            completion(Result(catching: {
+                try ManagedCache.find(in: context).map { cache in
+                    CachedFeed(
+                        feed: cache.localFeed, timestamp: cache.timestamp)
+                }
+            }))
+        }
+    }
+}
