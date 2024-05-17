a3af232402a3dcf0719d14a37d850d02e1f170fd
gtsofa <jlsnyule@yahoo.com>
2024-05-17 21:37:09 +0300

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` loads from primary loader first

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -35,30 +35,35 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
     }
     
-//    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
-//        let url = anyURL()
-//        let primaryLoader = LoaderSpy()
-//        let fallbackLoader = LoaderSpy()
-//        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
-//        
-//        _ = sut.loadImageData(from: url) { _ in }
-//        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
-//        XCTAssertEqual(fallbackLoader.loadedURLs, [], "Expected no loaded URLs from fallback loader")
-//    }
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        _ = sut.loadImageData(from: url) { _ in }
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+        XCTAssertEqual(fallbackLoader.loadedURLs, [], "Expected no loaded URLs from fallback loader")
+    }
     
     // MARK: - Helpers
     
     private func anyURL() -> URL {
-        return URL(string: "https://any-url.com")!
+        return URL(string: "https://a-url.com")!
     }
     private class LoaderSpy: FeedImageDataLoader {
-        var loadedURLs = [URL]()
+        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedURLs: [URL] {
+            return messages.map { $0.url }
+        }
         
         private struct Task: FeedImageDataLoaderTask {
             func cancel() {
             }
         }
         func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            messages.append((url, completion))
             return Task()
         }
     }
