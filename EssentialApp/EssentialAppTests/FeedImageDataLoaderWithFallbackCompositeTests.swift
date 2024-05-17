34f229d33e43566a4859636bb001fe3951e450bc
gtsofa <jlsnyule@yahoo.com>
2024-05-17 19:49:42 +0300

    Configure EssentialApp scheme to execute tests in random order and gather  coverage for the EssentialApp target

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -9,15 +9,19 @@ import XCTest
 import essential_feed_case_study
 
 public final class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    private let primary: FeedImageDataLoader
     private struct Task: FeedImageDataLoaderTask {
         func cancel() {
             
         }
     }
     
-    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {}
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        self.primary = primary
+    }
     
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        _ = primary.loadImageData(from: url) { _ in }
         return Task()
     }
 }
@@ -31,8 +35,22 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
     }
     
+//    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+//        let url = anyURL()
+//        let primaryLoader = LoaderSpy()
+//        let fallbackLoader = LoaderSpy()
+//        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+//        
+//        _ = sut.loadImageData(from: url) { _ in }
+//        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+//        XCTAssertEqual(fallbackLoader.loadedURLs, [], "Expected no loaded URLs from fallback loader")
+//    }
+    
     // MARK: - Helpers
     
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
     private class LoaderSpy: FeedImageDataLoader {
         var loadedURLs = [URL]()
         
