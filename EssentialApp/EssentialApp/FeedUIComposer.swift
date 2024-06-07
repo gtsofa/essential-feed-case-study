57bc46647e2c040a553df4fe8ec1fdce25eb67d3
gtsofa <jlsnyule@yahoo.com>
2024-06-07 11:34:05 +0300

    Move `dispatchOnMainQueue` operator to the point of use (before subscription with `sink`)

--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -15,14 +15,14 @@ public final class FeedUIComposer {
     // object creation
     public static func feedComposedWith(feedLoader:  @escaping () -> FeedLoader.Publisher, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(
-            feedLoader: { feedLoader().dispatchOnMainQueue() })
+            feedLoader: feedLoader)
         
         let feedController = makeFeedViewController(delegate: presentationAdapter, title: FeedPresenter.title)
         
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(
                 controller: feedController,
-                imageLoader: { imageLoader($0).dispatchOnMainQueue() }),
+                imageLoader: imageLoader),
             loadingView: WeakRefVirtualProxy(feedController),
             errorView: WeakRefVirtualProxy(feedController))
         //presenter.loadingView = WeakRefVirtualProxy(refreshController)
