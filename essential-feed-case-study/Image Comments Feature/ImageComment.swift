ddbef19adffd0aca5e681a61a47ed9916c59b838
gtsofa <jlsnyule@yahoo.com>
2024-06-10 13:52:24 +0300

    Add ImageComment data model

--- essential-feed-case-study/Image Comments Feature/ImageComment.swift
+++ essential-feed-case-study/Image Comments Feature/ImageComment.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+public struct ImageComment {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
