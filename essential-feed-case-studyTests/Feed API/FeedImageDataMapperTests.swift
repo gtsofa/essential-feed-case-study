d3917aa4841a176ebafcad5aaaf237d8d23cc126
gtsofa <jlsnyule@yahoo.com>
2024-06-12 12:13:24 +0300

    FeedImageDataMapper throws error on non 200HTTPResponse

--- essential-feed-case-studyTests/Feed API/FeedImageDataMapperTests.swift
+++ essential-feed-case-studyTests/Feed API/FeedImageDataMapperTests.swift
@@ -6,30 +6,16 @@
 //
 
 import XCTest
+import essential_feed_case_study
 
 final class FeedImageDataMapperTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_map_throwsErrorOnNon200HTTPResponse() throws {
+        let samples = [199, 201, 300, 400, 500]
+        
+        try samples.enumerated().forEach { index, code in
+            XCTAssertThrowsError(
+                try FeedImageDataMapper.map(anyData(), from: HTTPURLResponse(statusCode: code))
+            )
         }
     }
-
 }
