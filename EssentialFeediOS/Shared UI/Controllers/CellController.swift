31d09849795ab8aed89899c30fd6335e7f7d4ffb
gtsofa <jlsnyule@yahoo.com>
2024-07-01 02:31:26 +0300

    Replace protocol composition in a single type with a struct composition to prevent forcing clients to implement methods they don't care about

--- EssentialFeediOS/Shared UI/Controllers/CellController.swift
+++ EssentialFeediOS/Shared UI/Controllers/CellController.swift
@@ -5,4 +5,22 @@
 //  Created by Julius on 01/07/2024.
 //
 
-import Foundation
+import UIKit
+
+public struct CellController {
+    let dataSource: UITableViewDataSource
+    let delegate: UITableViewDelegate?
+    let dataSourcePrefetching: UITableViewDataSourcePrefetching?
+    
+    public init(_ dataSource: UITableViewDataSource & UITableViewDelegate & UITableViewDataSourcePrefetching) {
+        self.dataSource = dataSource
+        self.delegate = dataSource
+        self.dataSourcePrefetching = dataSource
+    }
+    
+    public init(_ dataSource: UITableViewDataSource) {
+        self.dataSource = dataSource
+        self.delegate = nil
+        self.dataSourcePrefetching = nil
+    }
+}
