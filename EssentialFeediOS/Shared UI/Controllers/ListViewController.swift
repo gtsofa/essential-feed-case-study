90f8c0c7d061335f9c46af9eaa067fcc9c0a380d
gtsofa <jlsnyule@yahoo.com>
2024-07-04 00:33:45 +0300

    Capture cell reference and load/reload cell resources on `willDisplayCell` as a fix for iOS 15 cell prefetching behavior change. On iOS 15+, for performance reasons, the table view data source may not recreate a cell using the `cellForRow` method if there's a cached cell for the given IndexPath. In this case, it'll just call `willDisplayCell`. However, we release a reference of the cell and cancel requests on `didEndDisplayingCell`. So, since `cellForRow` may not be called, we need to implement `willDisplayCell` to know when the cached cell is becoming visible again to recapture a reference of the cell and load/reload any resource needed for the cell.

--- EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -103,6 +103,11 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
         onViewIsAppearing?(self)
     }
     
+    public override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let dl = cellController(at: indexPath)?.delegate
+        dl?.tableView?(tableView, willDisplay: cell, forRowAt: indexPath)
+    }
+    
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         // tells imageloader to cancel an image data load from a url
         // cancel a task for agiven indexpath
