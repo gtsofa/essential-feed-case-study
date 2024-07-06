aafea8d89202371aecea50ec1dae3289345efe30
gtsofa <jlsnyule@yahoo.com>
2024-07-06 12:08:28 +0300

    Extract endpoint URL creation to the API modules

--- essential-feed-case-study/Image Comments API/ImageCommentsEndpoint.swift
+++ essential-feed-case-study/Image Comments API/ImageCommentsEndpoint.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+public enum ImageCommentsEndpoint {
+    case get(UUID)
+    
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case let .get(id):
+            return baseURL.appendingPathComponent("v1/image/\(id)/comments")
+        }
+    }
+}
