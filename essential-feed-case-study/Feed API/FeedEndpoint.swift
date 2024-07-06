be62a1310f2b543c4ced1382f6e24e9d1259e87d
gtsofa <jlsnyule@yahoo.com>
2024-07-06 12:07:47 +0300

    Extract endpoint URL creation to the API modules

---
+++ essential-feed-case-study/Feed API/FeedEndpoint.swift
@@ -0,0 +1,8 @@
+//
+//  FeedEndpoint.swift
+//  essential-feed-case-study
+//
+//  Created by Julius on 06/07/2024.
+//
+
+import Foundation
---
+++ essential-feed-case-study/Image Comments API/ImageCommentsEndpoint.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCommentsEndpoint.swift
+//  essential-feed-case-study
+//
+//  Created by Julius on 06/07/2024.
+//
+
+import Foundation
