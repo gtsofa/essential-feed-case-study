b488b959a0bc5e91b8ea095861fe0e5512f3f1c3
gtsofa <jlsnyule@yahoo.com>
2024-07-08 15:09:20 +0300

    Add `Load more` cell controller with loading indicator

--- EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
+++ EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -5,4 +5,25 @@
 //  Created by Julius on 08/07/2024.
 //
 
-import Foundation
+import UIKit
+import essential_feed_case_study
+
+public class LoadMoreCellController: NSObject, UITableViewDataSource {
+    private let cell = LoadMoreCell()
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        cell
+    }
+    
+    
+}
+
+extension LoadMoreCellController: ResourceLoadingView {
+    public func display(_ viewModel: ResourceLoadingViewModel) {
+        cell.isLoading = viewModel.isLoading
+    }
+}
--- EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
+++ EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
@@ -5,4 +5,29 @@
 //  Created by Julius on 08/07/2024.
 //
 
-import Foundation
+import UIKit
+
+public class LoadMoreCell: UITableViewCell {
+    private lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        contentView.addSubview(spinner)
+        
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            contentView.heightAnchor.constraint(lessThanOrEqualToConstant: 40)
+        ])
+        
+        return spinner
+    }()
+    
+    public var isLoading: Bool {
+        get { spinner.isAnimating }
+        set { if newValue {
+            spinner.startAnimating()
+        } else {
+            spinner.stopAnimating()
+        }}
+    }
+}
