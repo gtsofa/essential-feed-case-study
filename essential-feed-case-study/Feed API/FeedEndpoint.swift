38ada710bff255ee87394cb26223bd1665e866fb
gtsofa <jlsnyule@yahoo.com>
2024-07-09 22:15:20 +0300

    Set feed endpoint limit to 10

--- essential-feed-case-study/Feed API/FeedEndpoint.swift
+++ essential-feed-case-study/Feed API/FeedEndpoint.swift
@@ -9,11 +9,21 @@ import Foundation
 
 public enum FeedEndpoint {
     case get
-    
+    // if you're creating a library don't use optional URL
+    // if it's just a module then use option so when client pass a wrong url its a programmer error
     public func url(baseURL: URL) -> URL {
         switch self {
         case .get:
-            return baseURL.appendingPathComponent("v1/feed")
+            //let url = baseURL.appendingPathComponent("v1/feed")
+            var components = URLComponents()
+            components.scheme = baseURL.scheme
+            components.host = baseURL.host
+            components.path = baseURL.path + "/v1/feed"
+            components.queryItems = [
+                URLQueryItem(name: "limit", value: "10")
+            ]
+            
+            return components.url!
         }
     }
 }
