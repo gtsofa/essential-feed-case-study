af291c7ad832eaecd78ae959fe52fec19a320be3
gtsofa <jlsnyule@yahoo.com>
2024-07-17 19:13:25 +0300

    Move context.perform call up

--- essential-feed-case-studyTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
+++ essential-feed-case-studyTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
@@ -9,35 +9,35 @@ import Foundation
 
 protocol FeedStoreSpecs {
     
-    func test_retrieve_deliversEmptyOnEmptyCache()
-    func test_retrieveTwice_hasNoSideEffectsOnEmptyCache()
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
+    func test_retrieve_deliversEmptyOnEmptyCache() throws
+    func test_retrieveTwice_hasNoSideEffectsOnEmptyCache() throws
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() throws
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() throws
 
-    func test_insert_deliversNoErrorOnEmptyCache()
-    func test_insert_deliversNoErrorOnNonEmptyCache()
-    func test_insert_overridesPreviouslyInsertedCacheValues()
+    func test_insert_deliversNoErrorOnEmptyCache() throws
+    func test_insert_deliversNoErrorOnNonEmptyCache() throws
+    func test_insert_overridesPreviouslyInsertedCacheValues() throws
     
-    func test_delete_deliversNoErrorOnEmtpyCache()
-    func test_delete_hasNoSideEffectsOnEmtpyCache()
-    func test_delete_deliversNoErrorOnNonEmptyCache()
-    func test_delete_emptiesPreviouslyInsertedCache()
+    func test_delete_deliversNoErrorOnEmtpyCache() throws
+    func test_delete_hasNoSideEffectsOnEmtpyCache() throws
+    func test_delete_deliversNoErrorOnNonEmptyCache() throws
+    func test_delete_emptiesPreviouslyInsertedCache() throws
     
 }
 
 protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
-    func test_retrieve_dleiversFailureOnRetrievalError()
-    func test_retrieve_hasNoSideEffectsOnFailure()
+    func test_retrieve_dleiversFailureOnRetrievalError() throws
+    func test_retrieve_hasNoSideEffectsOnFailure() throws
 }
 
 protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs {
-    func test_insert_deliversErrorOnInsertionError()
-    func test_insert_hasNoSideEffectsOnInsertionError()
+    func test_insert_deliversErrorOnInsertionError() throws
+    func test_insert_hasNoSideEffectsOnInsertionError() throws
 }
 
 protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs {
-    func test_delete_deliversErrorOnDeletionError()
-    func test_delete_hasNoSideEffectsOnDeletionError()
+    func test_delete_deliversErrorOnDeletionError() throws
+    func test_delete_hasNoSideEffectsOnDeletionError() throws
 }
 
 // failable protocol specs composition
