a8c73524c82c34287f4f619b4c1ffe8dd7872ae8
gtsofa <jlsnyule@yahoo.com>
2024-07-22 23:52:54 +0300

    Remove performSync method

--- essential-feed-case-study/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ essential-feed-case-study/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -9,36 +9,24 @@ import Foundation
 
 extension CoreDataFeedStore: FeedStore {
     public func deleteCachedFeed() throws {
-        try performSync { context in
-            Result {
-                try ManagedCache.deleteCache(in: context)
-            }
-        }
+        try ManagedCache.deleteCache(in: context)
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {
-        try performSync { context in
-            Result {
-                // create a contex
-                //save the context
-                let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                managedCache.timestamp = timestamp
-                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-                
-                // try to save context
-                try context.save()
-            }
-        }
+        // create a contex
+        //save the context
+        let managedCache = try ManagedCache.newUniqueInstance(in: context)
+        managedCache.timestamp = timestamp
+        managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
+        
+        // try to save context
+        try context.save()
     }
     
     public func retrieve() throws -> CachedFeed? {
-        try performSync { context in
-            Result(catching: {
-                try ManagedCache.find(in: context).map { cache in
-                    CachedFeed(
-                        feed: cache.localFeed, timestamp: cache.timestamp)
-                }
-            })
+        try ManagedCache.find(in: context).map {
+            CachedFeed(
+                feed: $0.localFeed, timestamp: $0.timestamp)
         }
     }
 
