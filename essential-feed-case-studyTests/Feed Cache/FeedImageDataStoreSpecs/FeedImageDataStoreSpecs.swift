307e1a4d21aed0b96ef37a495364f3bf9c142be3
gtsofa <jlsnyule@yahoo.com>
2024-07-23 17:52:42 +0300

    Extract reusable FeedImageDataStoreSpecs

--- essential-feed-case-studyTests/Feed Cache/FeedImageDataStoreSpecs/FeedImageDataStoreSpecs.swift
+++ essential-feed-case-studyTests/Feed Cache/FeedImageDataStoreSpecs/FeedImageDataStoreSpecs.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+protocol FeedImageDataStoreSpecs {
+    func test_retrieveImageData_deliversNotFoundWhenEmpty() throws
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() throws
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() throws
+    func test_retrieveImageData_deliversLastInsertedValue() throws
+}
--- essential-feed-case-studyTests/Feed Cache/FeedImageDataStoreSpecs/XCTestCase+FeedImageDataStoreSpecs.swift
+++ essential-feed-case-studyTests/Feed Cache/FeedImageDataStoreSpecs/XCTestCase+FeedImageDataStoreSpecs.swift
@@ -6,29 +6,89 @@
 //
 
 import XCTest
+import essential_feed_case_study
 
-final class XCTestCase_FeedImageDataStoreSpecs: XCTestCase {
+extension FeedImageDataStoreSpecs where Self: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func assertThatRetrieveImageDataDeliversNotFoundOnEmptyCache(
+        on sut: FeedImageDataStore,
+        imageDataURL: URL = anyURL(),
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        expect(sut, toCompleteRetrievalWith: notFound(), for: imageDataURL, file: file, line: line)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func assertThatRetrieveImageDataDeliversNotFoundWhenStoredDataURLDoesNotMatch(
+        on sut: FeedImageDataStore,
+        imageDataURL: URL = anyURL(),
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let nonMatchingURL = URL(string: "http://a-non-matching-url.com")!
+
+        insert(anyData(), for: imageDataURL, into: sut, file: file, line: line)
+
+        expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL, file: file, line: line)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func assertThatRetrieveImageDataDeliversFoundDataWhenThereIsAStoredImageDataMatchingURL(
+        on sut: FeedImageDataStore,
+        imageDataURL: URL = anyURL(),
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let storedData = anyData()
+
+        insert(storedData, for: imageDataURL, into: sut, file: file, line: line)
+
+        expect(sut, toCompleteRetrievalWith: found(storedData), for: imageDataURL, file: file, line: line)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func assertThatRetrieveImageDataDeliversLastInsertedValueForURL(
+        on sut: FeedImageDataStore,
+        imageDataURL: URL = anyURL(),
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let firstStoredData = Data("first".utf8)
+        let lastStoredData = Data("last".utf8)
+
+        insert(firstStoredData, for: imageDataURL, into: sut, file: file, line: line)
+        insert(lastStoredData, for: imageDataURL, into: sut, file: file, line: line)
+
+        expect(sut, toCompleteRetrievalWith: found(lastStoredData), for: imageDataURL, file: file, line: line)
+    }
+
+}
+
+extension FeedImageDataStoreSpecs where Self: XCTestCase {
+
+    func notFound() -> Result<Data?, Error> {
+        .success(.none)
+    }
+
+    func found(_ data: Data) -> Result<Data?, Error> {
+        .success(data)
+    }
+
+    func expect(_ sut: FeedImageDataStore, toCompleteRetrievalWith expectedResult: Result<Data?, Error>, for url: URL,  file: StaticString = #filePath, line: UInt = #line) {
+        let receivedResult = Result { try sut.retrieve(dataForURL: url) }
+
+        switch (receivedResult, expectedResult) {
+        case let (.success( receivedData), .success(expectedData)):
+            XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+
+        default:
+            XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+        }
+    }
+
+    func insert(_ data: Data, for url: URL, into sut: FeedImageDataStore, file: StaticString = #filePath, line: UInt = #line) {
+        do {
+            try sut.insert(data, for: url)
+        } catch {
+            XCTFail("Failed to insert image data: \(data) - error: \(error)", file: file, line: line)
         }
     }
 
